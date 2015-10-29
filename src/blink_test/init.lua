95dbab4a2d8d35d77bb49fe71aa33ce5596d5aa6
Will <willblev@gmail.com>
2015-10-29 16:02:57 +0100

    changed timer indexes to 5 and 6 to avoid collisions

--- src/blink_test/init.lua
+++ src/blink_test/init.lua
@@ -2,26 +2,26 @@
 local pin = 4            --> equivalent to GPIO2
 local LED_state = gpio.LOW
 local time_between_blinks = 600    --> 1000ms = 1 second
---timer index to call blink function = 0
---timer index inside blink function = 1
+--timer index to call blink ON function = 5
+--timer index inside blink OFF function = 6
 
 
 -- Function which turns on LED then turns it off after (duration_of_blink) ms
 
--- Initialise the pin
+-- Initialise the pin and turn it off to begin with
 gpio.mode(pin, gpio.OUTPUT)
 gpio.write(pin, gpio.LOW)
 
 
 function ledON ()
       gpio.write(pin, gpio.HIGH)
-      tmr.alarm(6, 20, 0, ledOFF) 
+      tmr.alarm(5, 20, 0, ledOFF) 
     end
 
 
 function ledOFF ()
       gpio.write(pin, gpio.LOW)
-      tmr.alarm(1, time_between_blinks, 0, ledON) 
+      tmr.alarm(6, time_between_blinks, 0, ledON) 
     end
 
 
