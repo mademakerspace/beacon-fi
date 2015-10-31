75c273c1a7c1b448ea1175e045a1cc39f062b627
willblev <willblev@gmail.com>
2015-10-31 14:25:12 +0100

    blue LED functionality implemented

--- src/freewifi.lua
+++ src/freewifi.lua
@@ -1,7 +1,6 @@
 _DELAY_LIST = 3000
 _DELAY_BLINK = 250
-_TIMER_BLINK = 0
-_TIMER_LIST = 1
+_TIMER_SCAN = 1
 _PIN_LED = 4
 _TIME_BWTN_BLINKS_EXT=1200
 
@@ -22,34 +21,33 @@ function ledOFF()
   tmr.alarm(6, _TIME_BWTN_BLINKS_EXT, 0, ledON) 
 end
 
+function blueLED()
+	print('If I print.') 
+end                                      
+
 
 function listap(t)
   local foundFree = false
-  print('running listap func')
+ 
   for k,v in pairs(t) do
 	authmode, rssi, bssid, channel = string.match(v, "(%d),(-?%d+),(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x),(%d+)")
     isFree = authmode
     temp_strength = tonumber(rssi)
     if (isFree=="0") then
       foundFree = true
-      print('found an open network!')
-      print('strength is')
-      print(k)
-      print(rssi)
     end
   end
-  if (foundFree) then    
-  else
-    print('didnt find jack shit')
+  if (foundFree) then 
+		a=1   
   end
 end
 
 
 function repeatList()
-  print('running repeatList func')
+
   wifi.sta.getap(listap)
 end
-tmr.alarm(_TIMER_LIST, _DELAY_LIST, 1, repeatList)
-
+tmr.alarm(_TIMER_SCAN, _DELAY_LIST, 1, repeatList)
 ledON()
+tmr.alarm(4, 10000, 1, blueLED) 
 wifi.sta.getap(listap)
