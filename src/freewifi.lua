e27e3591cd549f083367a4be247bf8e4f51133e9
willblev <willblev@gmail.com>
2015-10-31 13:48:26 +0100

    it detects open APs and their strength

--- src/freewifi.lua
+++ src/freewifi.lua
@@ -1,69 +1,49 @@
---_DELAY_LIST delay between two AP lists
---_DELAY_BLINK blink delay if no free AP found
---_TIMER_BLINK id of blink alarm
---_TIMER_LIST id of list alarm
---_PIN_LED LED pin, nodemcu pin 4, ESP8266 pin GPIO2
--- values in ms for delays
 _DELAY_LIST = 3000
-_DELAY_BLINK = 2000
+_DELAY_BLINK = 250
 _TIMER_BLINK = 0
 _TIMER_LIST = 1
 _PIN_LED = 4
-_SIGNAL_STRENGTH = 500
--- strength is a transformation of -dB to range between 100ms and 2000ms
-_RAW_RSSI = 0
 
-wifi.setmode(wifi.STATION) -- sets the ESP into station mode
+
+wifi.setmode(wifi.STATION)
+
 
 gpio.mode(_PIN_LED, gpio.OUTPUT)
 gpio.write(_PIN_LED, gpio.LOW)
 
-ledState = false
 
-function blinker()
-  if (ledState==false) then
-    ledState = true
-    gpio.write(_PIN_LED, gpio.HIGH)        
-  else
-    ledState = false
-    gpio.write(_PIN_LED, gpio.LOW)
-  end
-end
 
---tmr.alarm( _TIMER_BLINK, _DELAY_BLINK, 1, blinker )
+
 
 function listap(t)
   local foundFree = false
-  for ssid,v in pairs(t) do
-    authmode, rssi, bssid, channel = string.match(v, "(%d),(-?%d+),(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x),(%d+)")
-    --print(ssid,authmode,rssi,bssid,channel)
-    --isFree= authmode
-    isFree=ssid
-    _RAW_RSSI= rssi
-    _SIGNAL_STRENGTH =  math.floor((rssi * rssi) / 5)
-	_DELAY_BLINK = _SIGNAL_STRENGTH
-    -- change ~= to == ; ~= is not  == is equal
-    if (isFree=="made-wing") then
+  print('running listap func')
+  for k,v in pairs(t) do
+	authmode, rssi, bssid, channel = string.match(v, "(%d),(-?%d+),(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x),(%d+)")
+    isFree = authmode
+    temp_strength = tonumber(rssi)
+    if (isFree=="0") then
       foundFree = true
+      print('found an open network!')
+      print('strength is')
+      print(rssi)
+      print(math.floor((temp_strength * temp_strength) / 5))
+      print(k)
+   
     end
   end
-  if (foundFree) then  
-    --print('WiFi network detected!') 
-    --print(_DELAY_BLINK)
-    tmr.stop(_TIMER_BLINK)
-    --gpio.write(_PIN_LED, gpio.HIGH)
-    tmr.alarm( _TIMER_BLINK, _SIGNAL_STRENGTH, 1, blinker )
+  if (foundFree) then    
   else
-    --print(':-( network not detected') -- this is how you print to serial connection ie TX
+    print('didnt find jack shit')
   end
 end
 
 
 function repeatList()
-  gpio.write(_PIN_LED, gpio.LOW)        
-  ledState = false
+  print('running repeatList func')
   wifi.sta.getap(listap)
 end
 tmr.alarm(_TIMER_LIST, _DELAY_LIST, 1, repeatList)
+
 
 wifi.sta.getap(listap)
--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,3 @@
 node.compile("freewifi.lua") 
---
 
 dofile("freewifi.lc")
